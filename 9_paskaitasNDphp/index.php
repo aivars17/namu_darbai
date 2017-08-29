697d7835425856a8a47b11bce00a8d3545af3a31
Unknown <amalosevskis@gmail.com>
2017-08-29 23:54:19 +0300

    dfsf

--- 9_paskaitasNDphp/index.php
+++ 9_paskaitasNDphp/index.php
@@ -8,12 +8,14 @@ function getTrucks($x,$y) {
 	$vnt = $x / $y ;
 	return ceil($vnt);
 }
-
+function kaina($x,$y){
+	return $x * $y;
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Namu darbai 9</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
@@ -32,25 +34,51 @@ function getTrucks($x,$y) {
 						<thead class="thead-inverse">
 							<tr >
 								<th class="text-center" colspan='4'>Baseinai</th>
+								<th class="text-center" colspan='4'>Autocisternų kiekis</th>
+							</tr>
+							<tr>
+								<th class="text-center" rowspan="2" >Ilgis (m.)</th>
+								<th class="text-center" rowspan="2" >Plotis (m.)</th>
+								<th class="text-center" rowspan="2" >Gylis (m.)</th>
+								<th class="text-center" rowspan="2" >Tūris (m3)</th>
+								<th class="text-center" colspan='2'>Baseinai</th>
 								<th class="text-center" colspan='2'>Autocisternų kiekis</th>
 							</tr>
 							<tr>
-								<th class="text-center" >Ilgis (m.)</th>
-								<th class="text-center" >Plotis (m.)</th>
-								<th class="text-center" >Gylis (m.)</th>
-								<th class="text-center" >Tūris (m3)</th>
+								
 								<th class="text-center" >10 (m3)</th>
+								<th class="text-center" >Kaina $</th>
 								<th class="text-center" >30 (m3)</th>
+								<th class="text-center" >Kaina $</th>
 							</tr>
 						</thead>
 						<tbody>
 							<?php 
 					
-							for ($r = 1; $r < 51; $r++) { 
+							for ($r = 1; $r <= 50; $r++) { 
 								$v = getVolume($r);
 								$T1 = getTrucks($v,10);
 								$T2 = getTrucks($v,30);
-								echo "<tr id='$r'><td>$r</td><td>3</td><td>1.5</td><td>$v</td><td>$T1 vnt</td><td>$T2 vnt</td></tr>";
+								$T1kaina = kaina($T1,100);
+								$T2kaina = kaina($T2,250);
+								$class10 = "";
+								$class30 = "";
+								if ($T1kaina < $T2kaina) {
+									$class10 = "table-success";
+									$class30 = "table-danger";
+								} else {
+									$class30 = "table-success";
+									$class10 = "table-danger";
+								}
+								echo "<tr id='$r'>
+									<td>$r</td>
+									<td>3</td><td>1.5</td>
+									<td>$v</td>
+									<td>$T1 vnt</td>
+									<td class='$class10'>$T1kaina</td>
+									<td>$T2 vnt</td>
+									<td class='$class30'>$T2kaina</td>
+									</tr>";
 							}
 							?>
 						</tbody>
